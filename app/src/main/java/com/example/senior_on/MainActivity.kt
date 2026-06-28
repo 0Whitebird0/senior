0b7eac4dd63e8eb12715c721d7c44c32096f15ce
Shin hanseok <113006166+0Whitebird0@users.noreply.github.com>
2026-06-28 20:28:54 +0900

    design: 빨간거 추가 (#5)

--- app/src/main/java/com/example/senior_on/MainActivity.kt
+++ app/src/main/java/com/example/senior_on/MainActivity.kt
@@ -10,7 +10,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.senior_on.ui.theme.Pink80
 import com.example.senior_on.ui.theme.SENIOR_ONTheme
 
 class MainActivity : ComponentActivity() {
